e69def90d1966cbbcf936e478a8669fc58888b04
Sahana M <sahanamurugeshp@gmail.com>
2026-02-07 16:38:00 +0530

    Add password generator functionality

---
+++ password_generator.py
@@ -0,0 +1,12 @@
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+print("Password Generator")
+
+length = int(input("Enter password length: "))
+print("Generated Password:", generate_password(length))
